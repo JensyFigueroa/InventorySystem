30e1145dbc80df59e3fc65f264ef83dc59e2f08b
Jensy Figueroa <jensy1418@gmail.com>
2024-01-30 19:41:50 -0500

    Se agrega en la capa de datos, 2 folder y 1 class IRepository y sus metodos

---
+++ InventorySystem.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.DataAccess.Repository.IRepository
+{
+    public interface IRepository <T> where T : class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null,
+            string includeProperty = null
+            );
+
+        IEnumerable<T> GetFirt(
+           Expression<Func<T, bool>> filter = null,
+           string includeProperty = null
+           );
+
+        void Add(T entities);
+        void remove(int id);
+        void remove(T entities);
+        void removeRange(IEnumerable<T> entities);
+
+
+    }
+}
