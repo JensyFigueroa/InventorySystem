5c11c9de1dfacf58752050f0cd19e24697dd259d
Jensy Figueroa <jensy1418@gmail.com>
2024-01-31 19:16:41 -0500

    Se genera la class y la interface Store

---
+++ InventorySystem.DataAccess/Repository/StoreRepository.cs
@@ -0,0 +1,33 @@
+﻿using InventorySystem.DataAccess.Data;
+using InventorySystem.DataAccess.Repository.IRepository;
+using InventorySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.DataAccess.Repository
+{
+    public class StoreRepository : Repository<Store>, IStoreRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public StoreRepository(ApplicationDbContext db) :base(db) 
+        {
+            _db = db;
+        }
+
+        public void Update(Store store)
+        {
+            var storeDB = _db.Stores.FirstOrDefault(s => s.Id == store.Id);
+            if (storeDB != null)
+            {
+                storeDB.Name = store.Name;
+                storeDB.Description = store.Description;
+                storeDB.State = store.State;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
